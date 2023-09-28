379e021472cebe1d6c72872c1403cc814e9b3a4f
Franak007 <franck.pertosa@gmail.com>
2023-09-28 15:49:10 +0200

    modif nft.service.ts to test hodt

--- src/app/service/nft.service.ts
+++ src/app/service/nft.service.ts
@@ -12,7 +12,7 @@ export class NftService {
   constructor(private http: HttpClient) { }
 
   getAllNfts(): Observable<INft[]> {
-    return this.http.get<INft[]>("https://127.0.0.1:8000/api/nfts");
+    return this.http.get<INft[]>("http://api-businesscasefp.atwebpages.com/api/nfts");
   }
 
 
